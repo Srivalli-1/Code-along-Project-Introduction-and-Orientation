b5e466a3d5ce87d98f0b8c7aa1df2da3cea69e31
Srivalli-1 <kajasrivalli5@gmail.com>
2025-09-08 12:16:20 +0530

    done

---
+++ src/com/school/Teacher.java
@@ -0,0 +1,17 @@
+package com.school;
+
+public class Teacher extends Person {
+    private String subject;
+
+    public Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    public String getSubject() { return subject; }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Teacher: " + getName() + ", Age: " + getAge() + ", Subject: " + subject);
+    }
+}
